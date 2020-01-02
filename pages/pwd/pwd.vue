833a961fa9a1519892fa6e80bda116ae84f00844
liangjunjie-home <792972988@qq.com>
2020-01-03 00:25:18 +0800

    优化项目

--- pages/pwd/pwd.vue
+++ pages/pwd/pwd.vue
@@ -1,53 +1,35 @@
 <template>
-    <view class="content">
-        <view class="input-group">
-            <view class="input-row">
-                <text class="title">邮箱：</text>
-                <m-input type="text" focus clearable v-model="email" placeholder="请输入邮箱"></m-input>
-            </view>
-        </view>
-
-        <view class="btn-row">
-            <button type="primary" class="primary" @tap="findPassword">提交</button>
-        </view>
+    <view class="container">
+	  <image src="/static/img/pwd.jpg" mode="widthFix"></image>
+	  <view class="text">请联系管理员</view>
     </view>
 </template>
 
 <script>
-    import service from '../../service.js';
-    import mInput from '../../components/m-input.vue';
-
+  
     export default {
-        components: {
-            mInput
-        },
         data() {
-            return {
-                email: ''
-            }
+            return{}
         },
         methods: {
-            findPassword() {
-                /**
-                 * 仅做示例
-                 */
-                if (this.email.length < 3 || !~this.email.indexOf('@')) {
-                    uni.showToast({
-                        icon: 'none',
-                        title: '邮箱地址不合法',
-                    });
-                    return;
-                }
-                uni.showToast({
-                    icon: 'none',
-                    title: '已发送重置邮件至注册邮箱，请注意查收。',
-                    duration: 3000
-                });
-            }
+           
         }
     }
 </script>
 
 <style>
-
+	.container{
+		position: relative;
+	}
+	.text{
+		text-align: center;
+		width: 100%;
+		position: absolute;
+		top: 40upx;
+		left: 0;
+		font-size: 18px;
+	}
+	image{
+	 width:100%;
+	}
 </style>
